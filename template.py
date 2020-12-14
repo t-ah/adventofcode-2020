e864f81c2975d2d2a8191cab16797043077975db
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2020-12-14 08:46:33 +0100

    14.1

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
 def read_input():
-    with open(__file__.replace("py", "txt"), "r") as f:
-        return f.read().split("\n")
+    with open(__file__[:5]+".txt", "r") as f:
+        return [x.split(" = ") for x in f.read().split("\n")]
 
 def main():
     lines = read_input()
