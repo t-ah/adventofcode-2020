0282003c641be506032240a6aa8afea9659f2566
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2020-12-26 10:50:34 +0100

    25

--- day25.py
+++ day25.py
@@ -1,9 +1,22 @@
-def read_input():
-    with open(__file__.replace("py", "txt"), "r") as f:
-        return f.read().split("\n")
+def find_loop_size(subj, result):
+    v = 1
+    i = 0
+    while True:
+        i += 1
+        v = transform(v, subj)
+        if v == result:
+            return i
+
+def transform(val, subj):
+    val *= subj
+    return val % 20201227
 
 def main():
-    lines = read_input()
+    card = 1327981
+    door = 2822615
+
+    l = find_loop_size(7, card)
+    print(transform(door, l))
 
 if __name__ == "__main__":
     main()
